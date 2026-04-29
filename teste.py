7944523a1cb114d620b41202bfae4e93f59d166c
Joana <joana.ramos@academico.ifpb.edu.br>
2026-04-29 11:23:26 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,5 +1,8 @@
 import streamlit as st
 
-st.image("teste.png")
-st.write("Site Dinaldo Jorge")
-st.link_button("Acessar", "https://www.dinaldo.com.br")
+st.image("gucci.empresa.webp")
+st.write("Joana Darc")
+st.image("foto.png.jpeg")
+st.write("Joana Darc, 17 ANOS. ESTUDO NO IFPB CAMPUS ITABAIANA, FAÇO O CURSO DE INFORMÁTICA E ESTOU NO 3° ANO.")
+st.image("ZAP.png")
+st.link_button("Acessar", "https://web.whatsapp.com/")
